c9f16bbe663f8548785b9030e0b182f8039a1460
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2025-07-24 18:28:16 +0530

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,12 @@
+#To install minikube first we require conntrack also
+#!/bin/bash
+sudo yum update
+
+sudo yum install docker -y
+sudo yum install conntrack -y
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+
+#To confirm the installation is done properly
+minikube version
